ae8f61a5650e8369015cb9f806a17343cdf02e2b
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-10-13 13:30:17 +0200

    Improved reading and merging of bibtex libraries and added generation of table with results

--- query-hit-processing.R
+++ query-hit-processing.R
@@ -19,13 +19,14 @@ existingDirs <- basePathVector[sapply(basePathVector, dir.exists)];
 ### Select first existing path as base path
 basePath <- existingDirs[1];
 repoPath <- file.path(basePath, 'scale-development-scripts');
+workingPath <- file.path(basePath, 'scale-development-scripts');
 
 ### Query hits path
 queryHitsPath <- file.path(repoPath, "sysrev");
 
 ### Filenames
-queryHitsFiles <- c("Query1and2.bib");
-queryHitsFiles <- c("temporary.bib");
+queryHitsFiles <- c("Query1and2.bib", "direct_hits.bib", "indirect_hits.bib");
+#queryHitsFiles <- c("temporary.bib");
 
 ################################################################################
 ### Load packages
@@ -33,28 +34,32 @@ queryHitsFiles <- c("temporary.bib");
 
 require('userfriendlyscience');
 require('metabefor');
-safeRequire('RefManageR');
+safeRequire('plyr');
 
 ################################################################################
 ### Import references
 ################################################################################
 
-queryHits <- list();
+queryHits <- lapply(queryHitsFiles,
+                    function(filename) {
+                      return(importBibtex(file.path(queryHitsPath, filename)));
+                    });
+names(queryHits) <- queryHitsFiles;
 
-# ### First read file to remove the JabRef comment
-# cleanFile <- readLines(file.path(queryHitsPath, queryHitsFiles));
-#
-# ### Paste all strings together
-# cleanFile <- paste(cleanFile, collapse="\n");
-#
-# ### Remove jabref comments
-# cleanFile <- gsub("(?s)@[Cc]omment\\{jabref-meta:[^\\}]*\\}", "", cleanFile, perl=TRUE);
-#
-# ### Write clean file to disk
-# writeLines(cleanFile, con=file.path(queryHitsPath, "tmp-clean-file.bib"));
-#
-# ### Import references
-# queryHits[['1and2']] <- ReadBib(file.path(queryHitsPath, "tmp-clean-file.bib"));
-queryHits[['1and2']] <- importBibtex(file.path(queryHitsPath, "tmp-clean-file.bib"));
-strsplit(queryHits[['1and2']]$records$operationalisations, "\\|\\|");
+resultsTable <- ldply(queryHits,
+                      function(queryHitsObject) {
+                        res <- data.frame(bibtexkey = queryHitsObject$records$bibtexkey,
+                                          author = queryHitsObject$records$author,
+                                          year = queryHitsObject$records$year)
+                        res$operationalisations <-
+                          sapply(strsplit(queryHitsObject$records$operationalisations, "\\|\\|"),
+                                 paste,
+                                 collapse="\n");
+                        return(res);
+                      });
+
+write.csv(resultsTable,
+          file = file.path(workingPath, "list of operationalisations.csv"),
+          row.names = FALSE);
+
 
