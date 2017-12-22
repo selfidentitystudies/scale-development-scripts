bdbb4f232448ebd20d61e1b8b9fd7cafaf963f79
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-12-22 16:27:10 +0100

    Big update while categorising operationalisations

--- query-hit-processing.R
+++ query-hit-processing.R
@@ -35,6 +35,7 @@ queryHitsFiles <- c("Query1and2.bib", "direct_hits.bib", "indirect_hits.bib");
 require('userfriendlyscience');
 require('metabefor');
 safeRequire('plyr');
+safeRequire('plyr');
 
 ################################################################################
 ### Import references
@@ -100,25 +101,81 @@ row.names(longResults) <- NULL;
 ###############################################################################
 ###############################################################################
 
-aspectStimulusFragments <- list(seeMyselfAs = "see myself as",
-                                importantPart = "important part",
-                                rarelyEvenThinkAbout = "rarely even think about",
-                                kindOfPerson = "kind of person",
-                                concernedAbout = "concerned about",
-                                feelAtALoss = "feel at a loss");
+aspectStimulusFragments <- list(importantPart = "important part",
+                                importantToMe = "important to me",
+                                believeIn = "believe in",
+                                meansMoreThan = "means more than",
+                                responsibility = "I have a responsibility",
+                                howIWantToLive = "want(ed)* to live",
+                                rarelyEvenThinkAbout = "rarely (even )*think about",
+                                kindOfPerson = c("kind of person", "type of (a )*person",
+                                                 "fits who they are", "typical (person|for me)", "out of character",
+                                                 "alien", "in my character", "I(.{1,2}m) a good example of",
+                                                 "profile of someone", "generally recognises"),
+                                considerMyself = "(class|consider|considered|see|saw) (my|your|them)sel(f|ves)",
+                                concernedAbout = "concerned (are you )*(about|with)",
+                                feelAtALoss = c("feel (that I missed out|lost|upset|a loss|at a loss)",
+                                                "los(t|e) something"),
+                                amSomeoneWho = c("I am someone who", "I am somebody who"),
+                                acceptibility = "accept",
+                                perceivedProjectedImage = c("might see me as", "people to see me as"),
+                                iAmA = c("I am a", "I am meat eater", "thinker", "person who teaches"),
+                                iAm = "I am",
+                                roleIdentity = "role identity",
+                                appropriateForMe = "appropriate for me",
+                                should = c("should"),
+                                miscellaneous = c("self-confident", "will study hard",
+                                                  "enjoyed", "to what extent .* affect you",
+                                                  "quite frankly", "normal part of everyday life",
+                                                  "part of my life",
+                                                  "most important issue(s)*",
+                                                  "I involve in", "feelings",
+                                                  "goals related to",
+                                                  "relevant to me", "each of us"),
+                                domainSpecific = c("read the list of"),
+                                selfAs = "self (as|to be)",
+                                role = c("role", "person primarily responsible"),
+                                embarrassed = "embarrassed",
+                                express = "express",
+                                lifestyle = "lifestyle",
+                                image = "image",
+                                values = "values",
+                                beingUnique = "being unique",
+                                important = "important",
+                                personal = "personal",
+                                characteristics = "characteristics",
+                                self = "self");
 
-invisible(sapply(seq_along(aspectStimulusFragments), function(i) {
+longResults$anyMatch <- FALSE;
+
+for (i in seq_along(aspectStimulusFragments)) {
   if (length(aspectStimulusFragments[[i]]) > 1) {
-    stop("Processing multiple matching regexes not implemented yet!");
+    longResults[, names(aspectStimulusFragments)[[i]]] <-
+      !longResults$anyMatch &
+      grepl(paste0(aspectStimulusFragments[[i]], collapse="|"),
+            longResults$operationalisation,
+            ignore.case=TRUE);
   } else {
-    longResults[, names(aspectStimulusFragments)[[i]]] <<-
-      grepl(aspectStimulusFragments[[i]], longResults$operationalisation, ignore.case=TRUE);
+    longResults[, names(aspectStimulusFragments)[[i]]] <-
+      !longResults$anyMatch &
+      grepl(aspectStimulusFragments[[i]],
+            longResults$operationalisation,
+            ignore.case=TRUE);
   }
-}));
-
-longResults$anyMatch <- apply(longResults[, names(aspectStimulusFragments)], 1, any);
+  longResults$anyMatch <- longResults$anyMatch | longResults[, names(aspectStimulusFragments)[[i]]];
+}
 
 multiVarFreq(longResults, names(aspectStimulusFragments));
 sum(longResults$anyMatch);
+nrow(longResults) - sum(longResults$anyMatch);
 
+#longResults[longResults$iAmA, 'operationalisation'];
+
+### Write to .csv file
+write.csv(longResults[!longResults$anyMatch, c('operationalisation',
+                                               'bibtexkey',
+                                               'author',
+                                               'title',
+                                               'year')],
+          file = file.path(workingPath, "operationalisations.csv"));
 
